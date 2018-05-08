1c9f33ae36c05d513dd09169f76164df4927f3b4
edom18 <edo.m18@gmail.com>
2018-05-08 15:00:03 +0900

    Added mover.

---
+++ Assets/ClippingPlaneSample/Scripts/CameraMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _target;
+
+	[SerializeField]
+	private float _distance;
+	
+	private void Update()
+	{
+		float x = Mathf.Cos(Time.time) * _distance;
+		float z = Mathf.Sin(Time.time) * _distance;
+		Vector3 pos = transform.position;
+		pos.x = x;
+		pos.z = z;
+		transform.position = _target.position + pos;
+		transform.LookAt(_target);
+	}
+}
---
+++ Assets/ClippingPlaneSample/Scripts/PlaneMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMover : MonoBehaviour
+{
+	[SerializeField]
+	private float _distance = 0.5f;
+
+	[SerializeField]
+	private float _speed = 1f;
+
+	private Vector3 _initPos;
+
+	private void Start()
+	{
+		_initPos = transform.position;
+	}
+	
+	private void Update()
+	{
+		float t = Mathf.Sin(Time.time * _speed);
+		Vector3 delta = transform.up * (t * _distance);
+		transform.position = _initPos + delta;
+	}
+}
